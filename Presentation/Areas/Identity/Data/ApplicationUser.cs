1ae917798b534152f3c6e489128c6c9b518b0741
Valeriy Sudakov <valeriy.sudakov@oalgroup.com>
2020-07-03 19:43:35 +0100

    app user info update added

---
+++ Presentation/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Presentation.Areas.Identity.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string AddressLine1 { get; set; } = "";
+
+        public string AddressLine2 { get; set; } = "";
+
+        public string PostCode { get; set; } = "";
+
+        public string City { get; set; } = "";
+
+        public string County { get; set; } = "";
+
+        public string Country { get; set; } = "";
+    }
+}
