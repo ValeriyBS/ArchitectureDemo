e06db56af86f3ad4dc87d30d9215c052a12e597f
Valeriy Sudakov <valeriy.sudakov@oalgroup.com>
2020-07-14 10:27:30 +0100

    Order Confirmation Email feature added

---
+++ Presentation/Areas/Identity/Components/UserMenu.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Areas.Identity.Components
+{
+    public class UserMenu : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
