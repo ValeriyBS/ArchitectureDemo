cf2a801513b4c6ea9272a88aebb9fc419af923c9
Valeriy Sudakov <valeriy.sudakov@oalgroup.com>
2020-07-30 12:45:23 +0100

    CreateOrdersListViewModelTests Added

---
+++ ApplicationTests/Orders/Commands/CreateOrdersListViewModel/Factory/OrdersListViewModelFactoryTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Application.Orders.Commands.CreateOrdersListViewModel.Factory;
+using AutoFixture;
+using Domain.Orders;
+using Tests.Core.AutoFixture;
+using Xunit;
+
+namespace Application.Tests.Orders.Commands.CreateOrdersListViewModel.Factory
+{
+    public class OrdersListViewModelFactoryTests
+    {
+        [Fact]
+        public void TestCreateShouldReturnOrdersListViewModel()
+        {
+            //Arrange
+            const int testPageSize = 2;
+            const int testPageIndex = 1;
+            var fixture = new OmitRecursionFixture();
+            var orders = fixture.CreateMany<Order>().ToList();
+            var sut = new OrdersListViewModelFactory();
+
+            //Act
+            var result = sut.Create(orders, testPageSize, testPageIndex);
+
+            //Assert
+            Assert.Equal(testPageSize, result.PageSize);
+            Assert.Equal(testPageIndex, result.PageIndex);
+            Assert.Equal(orders.Take(2), result.Orders);
+        }
+    }
+}
