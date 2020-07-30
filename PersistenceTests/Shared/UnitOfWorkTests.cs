86727635377e90a238d9e9d880c55014f55f1933
Valeriy Sudakov <valeriy.sudakov@oalgroup.com>
2020-07-30 09:43:14 +0100

    UnitOfWork Tests Added

--- PersistenceTests/Shared/UnitOfWorkTests.cs
+++ PersistenceTests/Shared/UnitOfWorkTests.cs
@@ -1,6 +1,26 @@
-﻿namespace Persistence.Tests.Shared
+﻿using System;
+using Moq;
+using Persistence.Shared;
+using Xunit;
+
+namespace Persistence.Tests.Shared
 {
     public class UnitOfWorkTests
     {
+        [Fact]
+        public void TestShouldSaveUnitOfWork()
+        {
+            //Arrange
+            var mockDatabaseContext = new Mock<IDatabaseContext>();
+
+
+            var sut = new UnitOfWork(mockDatabaseContext.Object);
+
+            //Act
+            sut.Save();
+
+            //Assert
+            mockDatabaseContext.Verify(d => d.Save(), Times.Once);
+        }
     }
 }
